f83beb454f4b63e793b1a84b1bdf8ccd1599e0de
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 11:51:26 -0400

    initial solution

--- src/main/java/ex01/base/ex01.java
+++ src/main/java/ex01/base/ex01.java
@@ -3,4 +3,14 @@ package ex01.base;
 import java.util.Scanner;
 
 public class ex01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+
+        String outputString = "Hello, " + name + ", it's a pleasure to meet you.";
+
+        System.out.println(outputString);
+    }
 }
